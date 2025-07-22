2a597c2df87de2c3d60221ad18994cfb6f72e90c
VEERESH069 <116574954+VEERESH069@users.noreply.github.com>
2025-07-22 17:26:52 +0530

    foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -5,7 +5,7 @@ import { protect } from '../middleware/authMiddleware.js';
 const foodRouter = express.Router();
 
 // Authentication
-router.get("/private", protect, (req, res) => {
+foodRouter.get("/private", protect, (req, res) => {
   res.send("This is a protected food route");
 });
 // Image Storage Engine
