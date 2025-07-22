7061657b6ca1b0fbf8a5ddb5e3368021a7fd4193
Sneha <snehatomar1205@gmail.com>
2025-07-22 15:14:24 +0530

    feat: add JWT authentication with register, login, and middleware (#5)

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+import authRouter from './routes/authRoute.js';
+
+// app config
+const app = express();
+const port = 4000;
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter);
+app.use("/api/auth", authRouter);
+
+
+app.get('/',(req,res)=>{
+    res.send("api working");
+});
+
+app.listen(port, ()=>{
+    console.log(`server started on port ${port}`);
+});
